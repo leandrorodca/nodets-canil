35fb180ab7a6f0733049e644682745cf64c2b4a2
leandrorodca <leandrorodca@gmail.com>
2021-08-15 19:35:06 -0300

    base do projeto

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-canil
+Projeto feito no curso da B7web
+
+### Pré-requisitos globais
+`npm -i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Rodar o projeto
+`npm run dev`
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+dotenv.config();
+
+const server = express();
+
+//sets
+server.set('view engine','mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+//uses
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+//routes
+server.use(mainRoutes);
+const port = process.env.PORT;
+server.listen(port,()=>console.log('Rodando na porta %d, iniciado em %s',port, Date() ));
